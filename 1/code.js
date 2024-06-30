df1ccf3fdbdf456c74165d8927e334374546e69a
sinskiy <sinskiy@proton.me>
2024-06-30 11:54:34 +0300

    refactor: add roundToCents() function

--- 1/code.js
+++ 1/code.js
@@ -3,13 +3,17 @@ getTip(100, 11.111111111);
 function getTip(bill, tip) {
   const tipNumber = percentageToNumber(tip);
   const tipAmount = bill * tipNumber;
-  const roundedTipAmount = getRoundedToPrecision(tipAmount, 2);
+  const roundedTipAmount = roundToCents(tipAmount);
   const totalAmount = bill + roundedTipAmount;
   console.log(`Tip: ${roundedTipAmount}, Total: ${totalAmount}`);
 }
 
 function percentageToNumber(percentage) {
-  return parseFloat(percentage) / 100;
+  return percentage / 100;
+}
+
+function roundToCents(number) {
+  return getRoundedToPrecision(number, 2);
 }
 
 function getRoundedToPrecision(number, precision) {
